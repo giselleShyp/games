c5c25d038151341004241530e4d58ad6bcc7390a
giselleShyp <alzawahragiselle@yahoo.com>
2024-05-22 14:15:32 +0300

    responsive

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
-
-import 'components/game.dart'; // Add this import
+import 'components/game.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+  Flame.device.setPortrait();
   runApp(
     const GameWidget.controlled(
-      gameFactory: MyPhysicsGame.new, // Modify this line
+      gameFactory: MyPhysicsGame.new,
     ),
   );
 }
